a84bc6c62a3298f954f0e34c1619531951d4c94b
JohnAntonusMaximus <johnradosta@gmail.com>
2016-05-18 17:36:53 -0500

    added in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -55,16 +56,22 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
+	db.todo.create(body).then(function(todo){
+		res.status(200).json(todo).send();
+	}).catch(function(e){
+		res.status(400).json(e).send();
+	});
 
-	body.description = body.description.trim();
-	body.id = todoNextId++;
-
-	todos.push(body);
-
-	res.json(body);
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
+	//
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
+	//
+	// todos.push(body);
+	//
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -113,6 +120,8 @@ app.put('/todos/:id', function(req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function() {
-	console.log('Express listening on port ' + PORT + '!');
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
