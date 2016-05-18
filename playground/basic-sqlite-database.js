8e7a47c6d4693351102f7dc107444d49c3fadd48
JohnAntonusMaximus <johnradosta@gmail.com>
2016-05-18 16:58:20 -0500

    first commit

---
+++ playground/basic-sqlite-database.js
@@ -0,0 +1,77 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined,{
+		'dialect': 'sqlite',
+		'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+	description: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		validate: {
+			len: [1,255]
+		}
+	},
+	completed: {
+		type: Sequelize.BOOLEAN,
+		allowNull: false,
+		defaultValue: false
+	}
+});
+
+	// Todo.create({
+	// 	description: 'Take out trash'
+	// }).then(function(){
+	// 	Todo.create({
+	// 		description: 'Clean Office'
+	// 	});
+	// });
+
+
+sequelize.sync().then(function(){
+	console.log('Everything is synced!');
+  return Todo.findAll({
+		where: {
+			description: {
+				$like: '%out%'
+			}
+		}
+	}).then(function(todos){
+		if(todos){
+			todos.forEach(function(todo){
+				console.log(todo.toJSON());
+			})
+		} else {
+			console.log('No Todo Found!');
+		}
+	}).catch(function(e){
+		console.log('Oops! Something went wrong: ',e)
+	});
+
+});
+
+// 	Todo.create({
+// 		description: 'Take out trash'
+// 	}).then(function(todo){
+// 		return Todo.create({
+// 			description: 'Clean Office'
+// 		});
+// 	}).then(function(){
+// 		//return Todo.findById(1);
+// 		return Todo.findAll({
+// 			where: {
+// 				description: {
+// 					$like:'%Office%'
+// 				}
+// 			}
+// 		});
+// 	}).then(function(todos){
+// 		if(todos) {
+// 			todos.forEach(function(todo){
+// 				console.log(todo.toJSON());
+// 			});
+// 		} else {console.log('No Todo Found!');}
+// 	}).catch(function(e){
+// 		console.log('Oops! Something went wrong: ', e);
+// 	});
+// });
