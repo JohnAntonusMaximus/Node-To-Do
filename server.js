ca549f4c676d2404d8ca739dba46918926294808
JohnAntonusMaximus <johnradosta@gmail.com>
2016-05-19 08:59:05 -0500

    converted GET /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -41,15 +41,16 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function(todo){
+		if(todo){
+			res.status(200).json(todo).send();
+		} else {
+			res.status(400).json(todo).send();
+		}
+	}, function(e){
+		 console.log("Oops! Something went wrong: ", e);
+	 	});
 });
 
 // POST /todos
@@ -62,16 +63,6 @@ app.post('/todos', function(req, res) {
 		res.status(400).json(e).send();
 	});
 
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-	//
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-	//
-	// todos.push(body);
-	//
-	// res.json(body);
 });
 
 // DELETE /todos/:id
