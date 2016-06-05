1c3389b70d5b840c06c4509ebe88448f64ee09ca
JohnAntonusMaximus <johnradosta@gmail.com>
2016-06-05 14:49:34 -0500

    Playing with associations in playground

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,59 +19,42 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
-	// Todo.create({
-	// 	description: 'Take out trash'
+var User = sequelize.define('user',{
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+	//force: true
+}).then(function(){
+	console.log('Everything is synced!');
+	
+	User.findById(1).then(function(user) {
+		user.getTodos({
+			where:{
+				completed: true
+			}
+	}).then(function(todos) {
+			todos.forEach(function(todo) {
+				console.log(todo.toJSON());
+				});
+		});
+	});
+	
+  });
+  
+	// User.create({
+	// 	email: 'johnnier@yahoo.com'
 	// }).then(function(){
-	// 	Todo.create({
-	// 		description: 'Clean Office'
+	// 	return Todo.create({
+	// 		description: 'Clean yard',
+	// 	})
+	// }).then(function(todo){
+	// 	User.findById(1).then(function(user){
+	// 		user.addTodo(todo);
 	// 	});
 	// });
 
 
-sequelize.sync().then(function(){
-	console.log('Everything is synced!');
-  return Todo.findAll({
-		where: {
-			description: {
-				$like: '%out%'
-			}
-		}
-	}).then(function(todos){
-		if(todos){
-			todos.forEach(function(todo){
-				console.log(todo.toJSON());
-			})
-		} else {
-			console.log('No Todo Found!');
-		}
-	}).catch(function(e){
-		console.log('Oops! Something went wrong: ',e)
-	});
-
-});
-
-// 	Todo.create({
-// 		description: 'Take out trash'
-// 	}).then(function(todo){
-// 		return Todo.create({
-// 			description: 'Clean Office'
-// 		});
-// 	}).then(function(){
-// 		//return Todo.findById(1);
-// 		return Todo.findAll({
-// 			where: {
-// 				description: {
-// 					$like:'%Office%'
-// 				}
-// 			}
-// 		});
-// 	}).then(function(todos){
-// 		if(todos) {
-// 			todos.forEach(function(todo){
-// 				console.log(todo.toJSON());
-// 			});
-// 		} else {console.log('No Todo Found!');}
-// 	}).catch(function(e){
-// 		console.log('Oops! Something went wrong: ', e);
-// 	});
-// });
